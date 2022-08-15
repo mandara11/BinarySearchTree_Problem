9229874238d3c327c310d801c94fcfa3706bf6a5
mandara11 <mandaram2345@gmail.com>
2022-08-15 23:01:00 +0530

    Added:UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BinarySearchTree Problem");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+
+            binarySearch.Display();
         }
     }
 }
